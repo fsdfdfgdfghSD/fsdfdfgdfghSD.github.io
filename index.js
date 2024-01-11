dfcd39639ce8e9696bc9a5d799bcc6f354a46d38
netheround <notmyemail@lol.com>
2024-01-11 21:54:08 +0200

    Fixed a bug

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = 6969
 
 const words = ["dimon", "net", "hamachi", "fignea polnaia", "awoo"]
 
-app.get("/dimonapi", (request, response) => {
+app.get("/", (request, response) => {
     response.status(200).send({
         word: words[Math.floor(Math.random() * words.length)]
     })
