e414ef5dafa172f4f199911b194e1d6d0582eaf3
fsdfdfgdfghSD <notmyemail@coolemail.com>
2024-01-11 19:46:28 +0200

    Removed the app.py file and added a new api.js

---
+++ api.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+const words = ["hamachi", "net", "fignea polnaia", "awoo", "dimon", "zima", "dimon gandon"]
+
+app.get("/dimonapi", (req, res) => {
+    const randomWord = words[Math.floor(Math.random() * words.length)]
+    res.json({ word: randomWord })
+})
+
+app.listen(port, () => {
+    console.log("Started!")
+})
